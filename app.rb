89904102c8555418d7605dd1931ff721b07640b1
mokkiko <maksim_pro@mail.ru>
2021-10-16 16:02:38 +0300

    iinit database, init @db

--- app.rb
+++ app.rb
@@ -2,6 +2,17 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'blog.db'
+  @db.results_as_hash
+end
+
+before do
+  init_db
+end
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -10,3 +21,9 @@ end
 get '/new_post' do
   erb :new_post
 end
+post '/new_post' do
+  @content = params[:content]
+  erb "you tiped #{@content}"
+
+end
+
