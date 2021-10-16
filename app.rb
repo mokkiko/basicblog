fc6bf01060be8a373c371bf98c365a34bf230887
mokkiko <maksim_pro@mail.ru>
2021-10-16 15:43:38 +0300

    add form and button submit

--- app.rb
+++ app.rb
@@ -7,6 +7,6 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
-get '/new' do
-  erb "Hello World"
+get '/new_post' do
+  erb :new_post
 end
